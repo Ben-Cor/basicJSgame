0297e306705534255fbe5802d7b6ba613b0c2e8e
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-04-25 15:29:11 +0100

    amended move function

--- src/main.js
+++ src/main.js
@@ -11,18 +11,18 @@ const character = {
       y: 10,
   },
   move: function(direction) {
-      if (direction === 'up') {
-          this.position.y -= 5;
-      }
-      else if (direction === 'down') {
-          this.position.y += 5;
-      }
-      else if (direction === 'left') {
-          this.position.x -= 5;
-      }
-      else if (direction === 'right') {
-          this.position.x += 5;
-      }
+    const step = 3; // Movement step size
+    const characterSize = 40; // Character size in pixels
+
+    if (direction === 'up' && this.position.y > 0) {
+        this.position.y -= step;
+    } else if (direction === 'down' && this.position.y < 110) {
+        this.position.y += step;
+    } else if (direction === 'left' && this.position.x > 0) {
+        this.position.x -= step;
+    } else if (direction === 'right' && this.position.x < 150) {
+        this.position.x += step;
+    }
   },
   hasKey: true,
   inventory: ['sword', 'shield', 'hat', 'potion'],
